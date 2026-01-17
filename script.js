193923c979234d6a7b9a6fc11ee567c05888561e
rdtanmoy09 <srtanmoy09@gmail.com>
2026-01-17 14:01:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let value = 0;
+
+function addno() {
+  value++;
+  document.getElementById("value").innerHTML = value;
+}
+
+function decno() {
+  value--;
+  document.getElementById("value").innerHTML = value;
+}
